a7fca0857a082e145d8540824f8f3164b5168ff9
bhambers <bhambers@gmail.com>
2025-07-11 12:25:35 +0100

    change avatar user profile icon

--- app/components/dashboard/Header.tsx
+++ app/components/dashboard/Header.tsx
@@ -24,8 +24,8 @@ export function Header({ onMenuClick }: HeaderProps) {
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
             <Input placeholder="Search anything..." className="pl-10 w-64" />
           </div>
-          <Avatar>
-            <AvatarImage src="/placeholder.svg?height=32&width=32" />
+          <Avatar className="p-1 bg-gray-200">
+            <AvatarImage src="/placeholder-user.jpg" alt="User Avatar" className="rounded-full" />
             <AvatarFallback>JD</AvatarFallback>
           </Avatar>
         </div>
